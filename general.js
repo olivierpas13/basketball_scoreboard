d58313a1abb2686a984fd8e763489b082765032e
Olivier Paspuel <olivier_paspuel@hotmail.com>
2022-07-09 09:08:25 -0500

    Practicing javascript in a basketball scoreboard

--- general.js
+++ general.js
@@ -6,32 +6,12 @@ let guestCountEl = document.querySelector(".guest-score")
 let newScoreHome = 0
 let newScoreGuest = 0
 
-function incrementOneHome(){
-    newScoreHome += 1
+function incrementHome(number){
+    newScoreHome += number
     homeCountEl.textContent = newScoreHome
 }
 
-function incrementTwoHome(){
-    newScoreHome += 2
-    homeCountEl.textContent = newScoreHome
-}
-
-function incrementThreeHome(){
-    newScoreHome += 3
-    homeCountEl.textContent = newScoreHome
-}
-
-function incrementOneGuest(){
-    newScoreGuest += 1
-    guestCountEl.textContent = newScoreGuest
-}
-
-function incrementTwoGuest(){
-    newScoreGuest += 2
-    guestCountEl.textContent = newScoreGuest
-}
-
-function incrementThreeGuest(){
-    newScoreGuest += 3
+function incrementGuest(number){
+    newScoreGuest += number
     guestCountEl.textContent = newScoreGuest
 }
